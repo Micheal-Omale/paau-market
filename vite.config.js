bb27095caa1fa6a9eeeba3b72971099a9a9fb1b4
Abel Emmanuel Enemona <mannydev02@gmail.com>
2023-10-12 19:59:05 +0100

    [fix] - Add backend url to vite for proxy.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://paaumarketserver.onrender.com/
-const baseURL = "http://localhost:5000";
+const baseURL =
+  "https://paaumarketserver.onrender.com/" || "http://localhost:5000";
 
 // https://vitejs.dev/config/
 export default defineConfig({
