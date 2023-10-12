1598b4b98bbe988319e02e8c7780441ed0ea188d
Abel Emmanuel Enemona <mannydev02@gmail.com>
2023-10-12 20:11:14 +0100

    Include categories

--- src/app/features/category/categorySlice.js
+++ src/app/features/category/categorySlice.js
@@ -4,6 +4,8 @@ const initialState = [
   { id: "1", category: "Phone" },
   { id: "2", category: "Gas Cylinder" },
   { id: "3", category: "Lodge" },
+  { id: "4", category: "Standing Fan" },
+  { id: "5", category: "Reading Table & Chair" },
 ];
 
 const categorySlice = createSlice({
