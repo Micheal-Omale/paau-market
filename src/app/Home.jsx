5fcc0bfb99807b94d7a2b8435d362c77388e2418
Abel Emmanuel Enemona <mannydev02@gmail.com>
2023-10-13 15:56:44 +0100

    [chore]: Open Graph

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -4,11 +4,6 @@ import Navbar from "./utilities/Navbar";
 import ProductsList from "./features/products/ProductsList";
 
 function Home() {
-  useEffect(() => {
-    document.title =
-      "PAAU Market - Online marketplace for student to buy or sell.";
-  }, []);
-
   return (
     <React.Fragment>
       <section className="bg-mantis-50 h-full">
